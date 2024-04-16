d5e46c46808b14f0fe7e1b9e08f7c1dbab7527c8
nurlansalkinbayev <nur_s72@mail.ru>
2024-04-16 11:17:09 +0500

    adding code that calculate exucution time

--- work_with_time.py
+++ work_with_time.py
@@ -1,9 +1,23 @@
 import time
+import random
 
 # получаем текущее локальное время в виде кортежа
-current_time=time.localtime()
+current_time = time.localtime()
 # преобразуем кортеж времени в строку
-time_str=time.asctime(current_time)
+time_str = time.asctime(current_time)
 
-print(time_str)
+# print(time_str)
 
+start_time = time.time()
+
+deck = list(range(1000, 10000))
+hand = random.sample(deck, k=200)
+# Сумма чисел в списке
+hand_sum = sum(hand)
+print(hand)
+print(hand_sum)
+end_time = time.time()
+
+execution_time = end_time - start_time
+
+print(f'Execution time: {execution_time} seconds.')
