efb70af2d221886bf52936798c192a715dd96210
nurlansalkinbayev <nur_s72@mail.ru>
2024-04-16 10:53:47 +0500

    adding comments

--- work_with_time.py
+++ work_with_time.py
@@ -1,7 +1,9 @@
 import time
 
+# получаем текущее локальное время в виде кортежа
 current_time=time.localtime()
-
+# преобразуем кортеж времени в строку
 time_str=time.asctime(current_time)
 
 print(time_str)
+
