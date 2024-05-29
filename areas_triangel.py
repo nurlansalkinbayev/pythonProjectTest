2d89d253e2fff2f060f7ba7ee8a50fcba8f7f5bf
nurlansalkinbayev <nur_s72@mail.ru>
2024-05-29 17:14:47 +0500

    create file how to calculate area of triangle using formula of Gerona

--- areas_triangel.py
+++ areas_triangel.py
@@ -1,5 +1,14 @@
-def calculate_area_triangle(lenght,height):
-     area=
+def calculate_area_triangle(lenght, height):
+    area = 0.5 * lenght * height
+    return area
 
 
+def main():
+    lenght = float(input('Введите длину основания: '))
+    height = float(input('Введите высоту отрезка опущенного на основание: '))
+    total_area = calculate_area_triangle(lenght, height)
+    print('Площадь треугольника:', total_area)
 
+
+if __name__ == '__main__':
+    main()
